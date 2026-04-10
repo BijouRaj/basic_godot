e78cccea9fb5b0ae54837178cc811027df3e7f08
Bijou <bijou.raj12@gmail.com>
2026-04-10 11:20:02 -0700

    added slowdown on death, falling through stage

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 60
 
@@ -10,6 +11,8 @@ var direction = 1
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
+		animated_sprite_2d.flip_h = true
 	elif ray_cast_left.is_colliding():
 		direction = 1
+		animated_sprite_2d.flip_h = false
 	position.x += direction * SPEED * delta
